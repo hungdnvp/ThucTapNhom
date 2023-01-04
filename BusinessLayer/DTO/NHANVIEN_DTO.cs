2b585cc782fa52438b27a0d634782c693e2d2430
phungdung111 <dungphung2302@gmail.com>
2023-01-04 17:21:53 +0700

    Them lop DTO class NhanVien.cs

---
+++ BusinessLayer/DTO/NHANVIEN_DTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.DTO
+{
+    public class NHANVIEN_DTO
+    {
+        public int MANV { get; set; }
+        public string HOTEN { get; set; }
+        public Nullable<bool> GIOITINH { get; set; }
+        public Nullable<System.DateTime> NGAYSINH { get; set; }
+        public string DIENTHOAI { get; set; }
+        public string CCCD { get; set; }
+        public string DIACHI { get; set; }
+        public byte[] HINHANH { get; set; }
+        public Nullable<int> IDPB { get; set; }
+        public string TENPB { get; set; }
+        public Nullable<int> IDBP { get; set; }
+        public string TENBP { get; set; }
+        public Nullable<int> IDCV { get; set; }
+        public string TENCV { get; set; }
+        public Nullable<int> IDTD { get; set; }
+        public string TENTD { get; set; }
+        public Nullable<int> IDDT { get; set; }
+        public string TENDT { get; set; }
+        public Nullable<int> IDTG { get; set; }
+        public string TENTG { get; set; }
+        public Nullable<int> MACTY { get; set; }
+        public bool? DATHOIVIEC { get; set; }
+    }
+}
