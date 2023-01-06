e11c03fe007b2837a9b3a14287c51e2c7478a8d6
Nguyễn Thảo Ngân <ngan.nguyen.0168@gmail.com>
2023-01-06 15:33:13 +0700

    KEY-23 <crud dantoc>

---
+++ BusinessLayer/DANTOC.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer;
+
+namespace BusinessLayer
+{
+    public class DANTOC
+    {
+        QLNHANSUEntities db = new QLNHANSUEntities();
+        public tb_DANTOC getItem(int id)
+        {
+            return db.tb_DANTOC.FirstOrDefault(x => x.ID == id);
+        }
+        public List<tb_DANTOC> getList()
+        {
+            return db.tb_DANTOC.ToList();
+        }
+        public tb_DANTOC Add(tb_DANTOC dt)
+        {
+            try
+            {
+                db.tb_DANTOC.Add(dt);
+                db.SaveChanges();
+                return dt;
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Lỗi: " + ex.Message);
+            }
+        }
+        public tb_DANTOC Update(tb_DANTOC dt)
+        {
+            try
+            {
+                var _dt = db.tb_DANTOC.FirstOrDefault(x => x.ID == dt.ID);
+                _dt.TENDT = dt.TENDT;
+                db.SaveChanges();
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Lỗi: " + ex.Message);
+            }
+        }
+        public void Delete(int id)
+        {
+            try
+            {
+                var _dt = db.tb_DANTOC.FirstOrDefault(x => x.ID == id);
+                db.tb_DANTOC.Remove(_dt);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Lỗi: " + ex.Message);
+            }
+        }
+    }
+}
