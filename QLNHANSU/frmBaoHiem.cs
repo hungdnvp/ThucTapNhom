7e76dcfd5aed7bbe58a213fc1957222f5a8b26e4
Quang Hùng Đỗ <doquanghung25102001@gmail.com>
2023-01-08 22:13:39 +0700

    KEY-36 <tao giao dien 1 - gird data>

---
+++ QLNHANSU/frmBaoHiem.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.DTO;
+using BusinessLayer;
+using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataLayer;
+
+namespace QLNHANSU
+{
+    public partial class frmBaoHiem : DevExpress.XtraEditors.XtraForm
+    {
+        public frmBaoHiem()
+        {
+            InitializeComponent();
+        }
+        
+    }
+}
