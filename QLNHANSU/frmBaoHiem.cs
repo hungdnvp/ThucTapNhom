1338d863acb9145fba4486db5c0aca8bb2b54bc0
Quang Hùng Đỗ <doquanghung25102001@gmail.com>
2023-01-08 22:15:06 +0700

    KEY-36 <load data , reset gui, show form>

--- QLNHANSU/frmBaoHiem.cs
+++ QLNHANSU/frmBaoHiem.cs
@@ -20,6 +20,49 @@ namespace QLNHANSU
         {
             InitializeComponent();
         }
-        
+        private void frmBaoHiem_Load(object sender, EventArgs e)
+        {
+            _bh = new BAOHIEM();
+            _nhanvien = new NHANVIEN();
+            _them = false;
+            _showHide(true);
+            loadData();
+            loadNhanVien();
+        }
+        void _showHide(bool bh)
+        {
+            btnLuu.Enabled = !bh;
+            btnHuy.Enabled = !bh;
+            btnThem.Enabled = bh;
+            btnSua.Enabled = bh;
+            btnXoa.Enabled = bh;
+            btnDong.Enabled = bh;
+            txtSoQD.Enabled = !bh;
+            txtLyDo.Enabled = !bh;
+            txtNoiDung.Enabled = !bh;
+            dtNgay.Enabled = !bh;
+            slkNhanVien.Enabled = !bh;
+
+
+
+        }
+        void _reset()
+        {
+            txtSoQD.Text = string.Empty;
+            txtLyDo.Text = string.Empty;
+            txtNoiDung.Text = string.Empty;
+
+        }
+        void loadNhanVien()
+        {
+            slkNhanVien.Properties.DataSource = _nhanvien.getList();
+            slkNhanVien.Properties.ValueMember = "MANV";
+            slkNhanVien.Properties.DisplayMember = "HOTEN";
+        }
+        void loadData()
+        {
+            gcDanhSach.DataSource = _bh.getListFull(1);
+            gvDanhSach.OptionsBehavior.Editable = false;
+        }
     }
 }
