65e13dac4e62513ec7f1503f0495da651e0eb243
dolong0702 <longngocjddoox@gmail.com>
2023-01-09 11:00:52 +0700

    KEY-33 <frm TrinhDo>

---
+++ QLNHANSU/frmTrinhDo - Copy.cs
@@ -0,0 +1,119 @@
+﻿using BusinessLayer;
+using DataLayer;
+using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLNHANSU
+{
+    public partial class frmTrinhDo : DevExpress.XtraEditors.XtraForm
+    {
+        public frmTrinhDo()
+        {
+            InitializeComponent();
+        }
+        TRINHDO _trinhdo;
+        bool _them;
+        int _id;
+
+        private void frmTrinhDo_Load(object sender, EventArgs e)
+        {
+            _them = false;
+            _trinhdo = new TRINHDO();
+            _showHide(true);
+            loadData();
+        }
+        void _showHide(bool kt)
+        {
+            btnLuu.Enabled = !kt;
+            btnHuy.Enabled = !kt;
+            btnThem.Enabled = kt;
+            btnSua.Enabled = kt;
+            btnXoa.Enabled = kt;
+            btnDong.Enabled = kt;
+            btnIn.Enabled = kt;
+            txtTrinhDo.Enabled = !kt;
+        }
+        void loadData()
+        {
+            gcDanhSach.DataSource = _trinhdo.getList();
+            gvDanhSach.OptionsBehavior.Editable = false;
+        }
+
+        private void btnThem_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            _them = true;
+            _showHide(false);
+            txtTrinhDo.Text = string.Empty;
+        }
+
+        private void btnSua_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            _them = false;
+            _showHide(false);
+        }
+
+        private void btnXoa_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có chắc chắn muốn xoá chứ ?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                _trinhdo.Delete(_id);
+                loadData();
+            }
+        }
+
+        private void btnLuu_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            SaveData();
+            loadData();
+            _them = false;
+            _showHide(true);
+        }
+
+        private void btnHuy_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            _them = false;
+            _showHide(true);
+            txtTrinhDo.Text = string.Empty;
+        }
+
+        private void btnIn_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+
+        }
+
+        private void btnDong_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            this.Close();
+        }
+        void SaveData()
+        {
+            if (_them)
+            {
+                tb_TRINHDO td = new tb_TRINHDO();
+                td.TENTD = txtTrinhDo.Text;
+                _trinhdo.Add(td);
+            }
+            else
+            {
+                var td = _trinhdo.getItem(_id);
+                td.TENTD = txtTrinhDo.Text;
+                _trinhdo.Update(td);
+            }
+
+        }
+
+        private void gvDanhSach_Click(object sender, EventArgs e)
+        {
+            _id = int.Parse(gvDanhSach.GetFocusedRowCellValue("IDTD").ToString());
+            txtTrinhDo.Text = gvDanhSach.GetFocusedRowCellValue("TENTD").ToString();
+        }
+    }
+}
