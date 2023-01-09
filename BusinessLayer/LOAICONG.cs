559842c3b726e9f35f294d215cdde99ade7c0d74
dolong0702 <longngocjddoox@gmail.com>
2023-01-09 08:31:44 +0700

    KEY-28 <Func LoaiCong>

---
+++ BusinessLayer/LOAICONG.cs
@@ -0,0 +1,68 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class LOAICONG
+    {
+        QLNHANSUEntities db = new QLNHANSUEntities();
+        public tb_LOAICONG getItem(int id)
+        {
+            return db.tb_LOAICONG.FirstOrDefault(x => x.IDLC == id);
+        }
+        public List<tb_LOAICONG> getList()
+        {
+            return db.tb_LOAICONG.ToList();
+        }
+        public tb_LOAICONG Add(tb_LOAICONG lc)
+        {
+            try
+            {
+                db.tb_LOAICONG.Add(lc);
+                db.SaveChanges();
+                return lc;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Lỗi: " + ex.Message);
+            }
+        }
+        public tb_LOAICONG Update(tb_LOAICONG lc)
+        {
+            try
+            {
+                var _lc = db.tb_LOAICONG.FirstOrDefault(x => x.IDLC == lc.IDLC);
+                _lc.TENLC = lc.TENLC;
+                _lc.HESO = lc.HESO;
+                _lc.UPDATED_BY = 1;
+                _lc.UPDATED_DATE = DateTime.Now;
+                db.SaveChanges();
+                return lc;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Lỗi: " + ex.Message);
+            }
+        }
+        public void Delete(int id)
+        {
+            try
+            {
+                var _lc = db.tb_LOAICONG.FirstOrDefault(x => x.IDLC == id);
+                db.tb_LOAICONG.Remove(_lc);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Lỗi: " + ex.Message);
+            }
+        }
+    }
+}
