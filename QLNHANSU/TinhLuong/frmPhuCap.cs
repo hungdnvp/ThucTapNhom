e4f90d7c29da2c2f660dcd2e02b1ceb029a93151
Developer Hung Mu Coi <76393322+devhungmucoi@users.noreply.github.com>
2023-01-10 08:34:56 +0700

    KEY-20 <frm PhuCap>

---
+++ QLNHANSU/TinhLuong/frmPhuCap.cs
@@ -0,0 +1,168 @@
+﻿using BusinessLayer;
+using DataLayer;
+using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLNHANSU.TinhLuong
+{
+    public partial class frmPhuCap : DevExpress.XtraEditors.XtraForm
+    {
+        public frmPhuCap()
+        {
+            InitializeComponent();
+        }
+        PHUCAP _phucap;
+        NHANVIEN _nv;
+        bool _them;
+        int _id;
+
+        private void frmPhuCap_Load(object sender, EventArgs e)
+        {
+            _them = false;
+            _phucap = new PHUCAP();
+            _nv = new NHANVIEN();
+            _showHide(true);
+            loadData();
+            loadNhanVien();
+            loadPhuCap();
+            cboPhuCap.SelectedIndexChanged += CboPhuCap_SelectedIndexChanged; ;
+        }
+
+        private void CboPhuCap_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var pc = _phucap.getItemPC(int.Parse(cboPhuCap.SelectedValue.ToString()));
+            if (pc != null)
+            {
+                spSoTien.EditValue = pc.SOTIEN;
+            }
+        }
+
+        void _showHide(bool kt)
+        {
+            btnLuu.Enabled = !kt;
+            btnHuy.Enabled = !kt;
+            btnThem.Enabled = kt;
+            btnSua.Enabled = kt;
+            btnXoa.Enabled = kt;
+            btnDong.Enabled = kt;
+            btnIn.Enabled = kt;
+            txtNoiDung.Enabled = !kt;
+            spSoTien.Enabled = !kt;
+            lkNhanVien.Enabled = !kt;
+            cboPhuCap.Enabled = !kt;
+        }
+        void loadNhanVien()
+        {
+            lkNhanVien.Properties.DataSource = _nv.getListFull();
+            lkNhanVien.Properties.DisplayMember = "HOTEN";
+            lkNhanVien.Properties.ValueMember = "MANV";
+        }
+        void loadPhuCap()
+        {
+            cboPhuCap.DataSource = _phucap.getListPC();
+            cboPhuCap.DisplayMember = "TENPC";
+            cboPhuCap.ValueMember = "IDPC";
+        }
+        void loadData()
+        {
+            gcDanhSach.DataSource = _phucap.getListFull();
+            gvDanhSach.OptionsBehavior.Editable = false;
+        }
+
+        private void btnThem_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            _them = true;
+            _showHide(false);
+            txtNoiDung.Text = string.Empty;
+            spSoTien.EditValue = 0;
+            lkNhanVien.EditValue = 0;
+            cboPhuCap.SelectedIndex = 0;
+        }
+
+        private void btnSua_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            _them = false;
+            _showHide(false);
+        }
+
+        private void btnXoa_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có chắc chắn xóa không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                _phucap.Delete(_id);
+                loadData();
+            }
+        }
+
+        private void btnLuu_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            SaveData();
+            loadData();
+            _them = false;
+            _showHide(true);
+        }
+
+        private void btnHuy_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            _them = false;
+            _showHide(true);
+            txtNoiDung.Text = string.Empty;
+        }
+
+        private void btnDong_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            this.Close();
+        }
+        void SaveData()
+        {
+            if (_them)
+            {
+                tb_NHANVIEN_PHUCAP pc = new tb_NHANVIEN_PHUCAP();
+                pc.IDPC = int.Parse(cboPhuCap.SelectedValue.ToString());
+                pc.SOTIEN = int.Parse(spSoTien.EditValue.ToString());
+                pc.MANV = int.Parse(lkNhanVien.EditValue.ToString());
+                pc.NOIDUNG = txtNoiDung.Text;
+                pc.NGAY = DateTime.Now;
+                pc.CREATED_BY = 1;
+                pc.CREATED_DATE = DateTime.Now;
+                _phucap.Add(pc);
+            }
+            else
+            {
+                var pc = _phucap.getItem(_id);
+                pc.IDPC = int.Parse(cboPhuCap.SelectedValue.ToString());
+                pc.SOTIEN = int.Parse(spSoTien.EditValue.ToString());
+                pc.MANV = int.Parse(lkNhanVien.EditValue.ToString());
+                pc.NOIDUNG = txtNoiDung.Text;
+                pc.NGAY = DateTime.Now;
+                pc.UPDATED_BY = 1;
+                pc.UPDATED_DATE = DateTime.Now;
+                _phucap.Update(pc);
+
+            }
+        }
+
+        private void gvDanhSach_Click(object sender, EventArgs e)
+        {
+            if (gvDanhSach.RowCount > 0)
+            {
+                _id = int.Parse(gvDanhSach.GetFocusedRowCellValue("ID").ToString());
+                txtNoiDung.Text = gvDanhSach.GetFocusedRowCellValue("NOIDUNG").ToString();
+                spSoTien.EditValue = gvDanhSach.GetFocusedRowCellValue("SOTIEN");
+                lkNhanVien.EditValue = gvDanhSach.GetFocusedRowCellValue("MANV");
+                cboPhuCap.SelectedValue = gvDanhSach.GetFocusedRowCellValue("IDPC");
+
+            }
+        }
+
+        
+    }
+}
