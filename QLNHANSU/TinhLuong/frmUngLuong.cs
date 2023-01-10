07698585129aeb600f34b6d8a87b5e432e64fca1
Developer Hung Mu Coi <76393322+devhungmucoi@users.noreply.github.com>
2023-01-10 08:36:07 +0700

    KEY-20 <frm UngLuong>

---
+++ QLNHANSU/TinhLuong/frmUngLuong.cs
@@ -0,0 +1,148 @@
+﻿using BusinessLayer;
+using DataLayer;
+using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLNHANSU.TinhLuong
+{
+    public partial class frmUngLuong : DevExpress.XtraEditors.XtraForm
+    {
+        public frmUngLuong()
+        {
+            InitializeComponent();
+        }
+        UNGLUONG _ul;
+        NHANVIEN _nv;
+        bool _them;
+        int _id;
+        private void frmUngLuong_Load(object sender, EventArgs e)
+        {
+            _them = false;
+            _nv = new NHANVIEN();
+            _ul = new UNGLUONG();
+            _showHide(true);
+            loadData();
+            loadNhanVien();
+        }
+        void _showHide(bool kt)
+        {
+            btnLuu.Enabled = !kt;
+            btnHuy.Enabled = !kt;
+            btnThem.Enabled = kt;
+            btnSua.Enabled = kt;
+            btnXoa.Enabled = kt;
+            btnDong.Enabled = kt;
+            btnIn.Enabled = kt;
+            txtNoiDung.Enabled = !kt;
+            spSoTien.Enabled = !kt;
+            lkNhanVien.Enabled = !kt;
+        }
+        void loadNhanVien()
+        {
+            lkNhanVien.Properties.DataSource = _nv.getListFull();
+            lkNhanVien.Properties.DisplayMember = "HOTEN";
+            lkNhanVien.Properties.ValueMember = "MANV";
+        }
+        void loadData()
+        {
+            gcDanhSach.DataSource = _ul.getListFull();
+            gvDanhSach.OptionsBehavior.Editable = false;
+        }
+
+        private void btnThem_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            _them = true;
+            _showHide(false);
+            txtNoiDung.Text = string.Empty;
+            spSoTien.EditValue = 0;
+            lkNhanVien.EditValue = 0;
+        }
+
+        private void btnSua_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            _them = false;
+            _showHide(false);
+        }
+
+        private void btnXoa_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có chắc chắn xóa không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                _ul.Delete(_id);
+                loadData();
+            }
+        }
+
+        private void btnLuu_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            SaveData();
+            loadData();
+            _them = false;
+            _showHide(true);
+        }
+
+        private void btnHuy_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            _them = false;
+            _showHide(true);
+            txtNoiDung.Text = string.Empty;
+        }
+
+        private void btnDong_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            this.Close();
+        }
+        void SaveData()
+        {
+            if (_them)
+            {
+                tb_UNGLUONG ul = new tb_UNGLUONG();
+                ul.SOTIEN= int.Parse(spSoTien.EditValue.ToString());
+                ul.MANV = int.Parse(lkNhanVien.EditValue.ToString());
+                ul.GHICHU = txtNoiDung.Text;
+                ul.NGAY = DateTime.Now.Day;
+                ul.THANG = DateTime.Now.Month;
+                ul.NAM = DateTime.Now.Year;
+
+                ul.CREATED_BY = 1;
+                ul.CREATED_DATE = DateTime.Now;
+                _ul.Add(ul);
+            }
+            else
+            {
+                var ul = _ul.getItem(_id);
+                ul.SOTIEN = int.Parse(spSoTien.EditValue.ToString());
+                ul.MANV = int.Parse(lkNhanVien.EditValue.ToString());
+                ul.GHICHU = txtNoiDung.Text;
+                ul.NGAY = DateTime.Now.Day;
+                ul.THANG = DateTime.Now.Month;
+                ul.NAM = DateTime.Now.Year;
+
+                ul.UPDATED_BY = 1;
+                ul.UPDATED_DATE = DateTime.Now;
+                _ul.Update(ul);
+
+            }
+        }
+
+        private void gvDanhSach_Click(object sender, EventArgs e)
+        {
+            if (gvDanhSach.RowCount > 0)
+            {
+                _id = int.Parse(gvDanhSach.GetFocusedRowCellValue("ID").ToString());
+                txtNoiDung.Text = gvDanhSach.GetFocusedRowCellValue("GHICHU").ToString();
+                spSoTien.EditValue = gvDanhSach.GetFocusedRowCellValue("SOTIEN");
+                lkNhanVien.EditValue = gvDanhSach.GetFocusedRowCellValue("MANV");
+
+            }
+        }
+    }
+}
